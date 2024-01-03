c93bcabebeb8f5b00c8eab3532ef525edb7b0606
Srestha2809 <105350880+Srestha2809@users.noreply.github.com>
2024-01-02 19:33:57 -0500

    Delete arrow_example.js

--- arrow_example.js
+++
@@ -1,9 +0,0 @@
-let multiply = (x, y) => {
-    return x * y;
-};
-
-console.log(`eS6+ answer = ${multiply(5, 10)}`);
-
-let multiply1 = (x, y) => { x * y };
-
-console.log(`eS6+ answer = ${multiply(5, 10)}`);
