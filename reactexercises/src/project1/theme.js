b936e9c64103e4148785ba1e9c44fb6e50a9cfc9
Srestha2809 <105350880+Srestha2809@users.noreply.github.com>
2024-01-02 19:31:55 -0500

    Delete reactexercises/src/project1 directory

--- reactexercises/src/project1/theme.js
+++
@@ -1,7 +0,0 @@
-import { createTheme } from "@mui/material/styles";
-export default createTheme({
-    typography: {
-        useNextVariants: true,
-    },
-    palette: { "common": { "black": "rgba(37, 36, 36, 1)", "white": "rgba(156, 5, 5, 1)" }, "background": { "paper": "#fff", "default": "#fafafa" }, "primary": { "light": "#7986cb", "main": "rgba(9, 19, 75, 1)", "dark": "#303f9f", "contrastText": "#fff" }, "secondary": { "light": "#ff4081", "main": "#f50057", "dark": "#c51162", "contrastText": "#fff" }, "error": { "light": "#e57373", "main": "#f44336", "dark": "#d32f2f", "contrastText": "#fff" }, "text": { "primary": "rgba(0, 0, 0, 0.87)", "secondary": "rgba(0, 0, 0, 0.54)", "disabled": "rgba(0, 0, 0, 0.38)", "hint": "rgba(0, 0, 0, 0.38)" } }
-});
