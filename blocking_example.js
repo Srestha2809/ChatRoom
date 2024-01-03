259973f287238cd17de7014eb8ff0db7a852194a
Srestha2809 <105350880+Srestha2809@users.noreply.github.com>
2024-01-02 19:34:05 -0500

    Delete blocking_example.js

--- blocking_example.js
+++
@@ -1,5 +0,0 @@
-let fs = require('fs');
-let users = fs.readFileSync('./sampledata/users', 'utf-8');
-console.log(`${users}\n`);
-let emails = fs.readFileSync('./sampledata/emailaddresses', 'utf-8');
-console.log(emails);
