e7c7cc7910715241281ed6ed8df191293dfab049
Srestha2809 <105350880+Srestha2809@users.noreply.github.com>
2024-01-02 19:33:50 -0500

    Delete arrow_example2.js

--- arrow_example2.js
+++
@@ -1,3 +0,0 @@
-let setValues = (id, name, age) => ({ id: id, name: name, age: age });
-let student = setValues(3, "Srestha", 21);
-console.log(`Id: ${student.id}, Name: ${student.name}, Age: ${student.age}`);
