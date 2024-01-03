f9ca0cb97994c893cea2a596abcfb4ac05302e59
Srestha2809 <105350880+Srestha2809@users.noreply.github.com>
2024-01-02 19:29:49 -0500

    Delete server directory

--- server/config.js
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-config();
-export const atlas = process.env.DBURL;
-export const appdb = process.env.DB;
-export const basicinfoCollection = process.env.BASICINFOCOLLECTION;
-export const memberCollection = process.env.MEMBERCOLLECTION;
-export const backlogCollection = process.env.BACKLOGCOLLECTION;
-export const port = process.env.PORT;
