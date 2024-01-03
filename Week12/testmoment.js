f8bfc5fc3f4e0b3da0864a44ec15b6e2c4b12e94
Srestha2809 <105350880+Srestha2809@users.noreply.github.com>
2024-01-02 19:27:03 -0500

    Delete Week12 directory

--- Week12/testmoment.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-// default format
-console.log(moment().format(''))
-// just the time
-console.log(moment().format('h:mm:ss a'))
-// just the date
-console.log(moment().format('YYYY-MM-DD'))
